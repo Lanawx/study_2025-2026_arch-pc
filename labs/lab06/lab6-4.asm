af6389d271319dd993ac674089cc590bceb13d92
Милана Мартынова <milanamartynova531@gmail.com>
2025-11-05 13:17:03 +0300

    Add files via upload

---
+++ labs/lab06/lab6-4.asm
@@ -0,0 +1,24 @@
+%include 'in_out.asm'
+SECTION .data
+msg: DB 'Введите значение переменной х: ',0
+rem: DB 'Результат: ',0
+SECTION .bss
+x: RESB 80
+SECTION .text
+GLOBAL _start
+_start:
+mov eax, msg
+call sprint
+mov ecx, x
+mov edx, 80
+call sread
+mov eax, x
+call atoi
+add eax, 2
+mul eax
+mov edi, eax
+mov eax, rem
+call sprint
+mov eax, edi
+call iprint
+call quit
